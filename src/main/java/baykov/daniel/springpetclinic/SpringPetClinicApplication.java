e63c083b96b4a3ed1662ac60f6729e451f5fabc6
Daniel Baykov <danibaikov@abv.bg>
2023-03-15 18:34:22 +0200

    initial commit

---
+++ src/main/java/baykov/daniel/springpetclinic/SpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package baykov.daniel.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicApplication.class, args);
+	}
+
+}
