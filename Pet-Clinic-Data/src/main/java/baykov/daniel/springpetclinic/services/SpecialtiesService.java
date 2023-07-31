ad33e426842225a53a4f41401cbbdef240e514f0
Daniel Baykov <danibaikov@gmail.com>
2023-07-31 10:30:52 +0300

    adding Specialties Map Impl. Closes #31

---
+++ Pet-Clinic-Data/src/main/java/baykov/daniel/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package baykov.daniel.springpetclinic.services;
+
+import baykov.daniel.springpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
