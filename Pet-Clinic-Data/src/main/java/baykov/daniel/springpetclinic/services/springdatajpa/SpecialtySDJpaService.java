feb747880b34e2749a91c77e74f00f32998609f3
Daniel Baykov <danibaikov@gmail.com>
2023-07-31 19:00:28 +0300

    adding spring data jpa service for specialty. Closes #44

---
+++ Pet-Clinic-Data/src/main/java/baykov/daniel/springpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,49 @@
+package baykov.daniel.springpetclinic.services.springdatajpa;
+
+import baykov.daniel.springpetclinic.model.Specialty;
+import baykov.daniel.springpetclinic.repositories.SpecialtyRepository;
+import baykov.daniel.springpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long specialtyId) {
+        return specialtyRepository.findById(specialtyId).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long specialtyId) {
+        specialtyRepository.deleteById(specialtyId);
+    }
+}
