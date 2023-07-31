79c74b3a960cfbf77ba89ed8d58c743f8f10bf79
Daniel Baykov <danibaikov@gmail.com>
2023-07-31 18:49:42 +0300

    adding spring data jpa service for vets. Closes #41

---
+++ Pet-Clinic-Data/src/main/java/baykov/daniel/springpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,49 @@
+package baykov.daniel.springpetclinic.services.springdatajpa;
+
+import baykov.daniel.springpetclinic.model.Vet;
+import baykov.daniel.springpetclinic.repositories.VetRepository;
+import baykov.daniel.springpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long vetId) {
+        return vetRepository.findById(vetId).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long vetId) {
+        vetRepository.deleteById(vetId);
+    }
+}
